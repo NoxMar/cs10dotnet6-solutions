ec00e896976f38cd31e99a3e0b8157d64df41e21
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-20 21:39:04 +0100

    feat: Do 3rd exercise from 3rd chapter

---
+++ solutions/Chapter03/Exercise03/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+const int PER_LINE = 10;
+const int MAX_NUMBER = 100;
+
+for (int i = 1; i < MAX_NUMBER; i += PER_LINE)
+{
+    var mappedNumbers = Enumerable.Range(i, PER_LINE)
+        .Select(i => FizzBuzz(i));
+    WriteLine(String.Join(", ", mappedNumbers));
+}
+
+string FizzBuzz(int i)
+{
+    return (i % 3, i % 5) switch
+    {
+        (0, 0) => "FizzBuzz",
+        (0, _) => "Fizz",
+        (_, 0) => "Buzz",
+        (_, _) => i.ToString()
+    };
+}
