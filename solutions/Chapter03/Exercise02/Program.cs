42dd5ba47ecd20ef61860d44064f12d1e98049cf
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-20 20:50:39 +0100

    feat: Do 2nd exercise from 3rd chapter

---
+++ solutions/Chapter03/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+try
+{
+    checked
+    {
+
+        int max = 500;
+        for (byte i = 0; i < max; i++)
+        {
+            WriteLine(i);
+        }
+    }
+}
+catch (OverflowException)
+{
+    Console.WriteLine($"Error: integer overflow encountered for `byte` variable, max value for this type is {byte.MaxValue}");
+}
