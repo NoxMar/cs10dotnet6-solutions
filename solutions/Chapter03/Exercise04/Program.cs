3a28670be7b3b4bf0ed0af6dfb46ca0b406d79d7
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-20 22:46:33 +0100

    feat: Do 4th exercise from 3rd chapter

---
+++ solutions/Chapter03/Exercise04/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+byte ReadByteWithPrompt(string prompt)
+{
+    Write(prompt);
+    return byte.Parse(ReadLine()!);
+}
+
+try
+{
+    byte dividend = ReadByteWithPrompt("Enter a number between 0 and 255:");
+    byte divisor = ReadByteWithPrompt("Enter another number between 0 and 255:");
+
+    WriteLine($"{dividend} divided by {divisor} is {dividend / divisor}");
+}
+catch (Exception e)
+{
+    WriteLine($"{e.GetType().Name}: {e.Message}");
+}
+
