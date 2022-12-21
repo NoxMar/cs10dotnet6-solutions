dd7e64569f428e3a47f7dc16a6b982ca2712ed3e
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-21 21:56:52 +0100

    feat: Add impreative Fibonacci example

--- solutions/Chapter04/WritingFunctions/Program.cs
+++ solutions/Chapter04/WritingFunctions/Program.cs
@@ -96,6 +96,33 @@ internal class Program
         }
     }
 
+    static int FibImperative(int term)
+    {
+        if (term == 1)
+        {
+            return 0;
+        }
+        else if (term == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return FibImperative(term - 1) + FibImperative(term - 2);
+        }
+    }
+
+    static void RunFibImperative()
+    {
+        for (int i = 1; i <= 30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}",
+                CardinalToOrdinal(i),
+                FibImperative(i));
+        }
+    }
+
+
     public static void Main(string[] args)
     {
         // TimesTable(12);
@@ -105,6 +132,8 @@ internal class Program
 
         // RunCardinalToOrdinal();
 
-        RunFactorial();
+        // RunFactorial();
+
+        RunFibImperative();
     }
 }
