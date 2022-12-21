c12b12f74e7333033a7c8692ee027c1c28fce9a6
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-21 19:26:46 +0100

    feat: add CalculatingTax from 4th chapter

--- solutions/Chapter04/WritingFunctions/Program.cs
+++ solutions/Chapter04/WritingFunctions/Program.cs
@@ -9,4 +9,49 @@ static void TimesTable(byte number)
     }
 }
 
-TimesTable(12);
+// TimesTable(12);
+
+
+static decimal CalculateTax(
+  decimal amount, string twoLetterRegionCode)
+{
+    decimal rate = 0.0M;
+    switch (twoLetterRegionCode)
+    {
+        case "CH": // Switzerland
+            rate = 0.08M;
+            break;
+        case "DK": // Denmark
+        case "NO": // Norway
+            rate = 0.25M;
+            break;
+        case "GB": // United Kingdom
+        case "FR": // France
+            rate = 0.2M;
+            break;
+        case "HU": // Hungary
+            rate = 0.27M;
+            break;
+        case "OR": // Oregon
+        case "AK": // Alaska
+        case "MT": // Montana
+            rate = 0.0M;
+            break;
+        case "ND": // North Dakota
+        case "WI": // Wisconsin
+        case "ME": // Maine
+        case "VA": // Virginia
+            rate = 0.05M;
+            break;
+        case "CA": // California
+            rate = 0.0825M;
+            break;
+        default: // most US states
+            rate = 0.06M;
+            break;
+    }
+    return amount * rate;
+}
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay} in tax.");
