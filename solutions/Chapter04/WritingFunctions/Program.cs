8ba5a94eaec7ef0deaddb57914da07a43a013b61
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-21 19:38:28 +0100

    feat: add CardinalToOrdinal example from chapter 4

--- solutions/Chapter04/WritingFunctions/Program.cs
+++ solutions/Chapter04/WritingFunctions/Program.cs
@@ -28,5 +28,37 @@ static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
     return amount * rate;
 }
 
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
-WriteLine($"You must pay {taxToPay} in tax.");
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay} in tax.");
+
+static string CardinalToOrdinal(int number)
+{
+    switch (number)
+    {
+        case 11:
+        case 12:
+        case 13:
+            return $"{number}th";
+        default:
+            int lastDigit = number % 10;
+            string suffix = lastDigit switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            };
+            return $"{number}{suffix}";
+    }
+}
+
+static void RunCardinalToOrdinal()
+{
+    for (int number = 1; number <= 40; number++)
+    {
+        Write($"{CardinalToOrdinal(number)} ");
+    }
+    WriteLine();
+}
+
+RunCardinalToOrdinal();
