e35d2a9e43c04c0fc552b82a8c42b6b705bac86a
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-21 19:21:52 +0100

    feat: Implement TimesTable function from the 4th chapter

---
+++ solutions/Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+static void TimesTable(byte number)
+{
+    WriteLine($"This is the {number} times table:");
+    for (byte row = 0; row <= 12; row++)
+    {
+        WriteLine($"{row} x {number} = {row * number}");
+    }
+}
+
+TimesTable(12);
