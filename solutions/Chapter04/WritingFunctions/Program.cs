13237078fa72a96350be13b4d03115c1e1b6548e
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-21 19:47:26 +0100

    feat: add Factorial example from chapter 4

--- solutions/Chapter04/WritingFunctions/Program.cs
+++ solutions/Chapter04/WritingFunctions/Program.cs
@@ -61,4 +61,41 @@ static void RunCardinalToOrdinal()
     WriteLine();
 }
 
-RunCardinalToOrdinal();
+// RunCardinalToOrdinal();
+
+static int Factorial(int number)
+{
+    if (number < 1)
+    {
+        return 0;
+    }
+    else if (number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        checked
+        {
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
+
+static void RunFactorial()
+{
+    for (int i = 1; i <= 14; i++)
+    {
+        try
+        {
+            WriteLine($"{i}! = {Factorial(i):N0}");
+        }
+        catch (System.OverflowException)
+        {
+            WriteLine($"{i}! is to big for a 32-bit integer.");
+        }
+    }
+}
+
+RunFactorial();
