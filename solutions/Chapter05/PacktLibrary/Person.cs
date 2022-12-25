240590325425dd6865ddef3dbce5246f49deb516
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-25 22:27:49 +0100

    feat: Add Person type to the PackLibrary

---
+++ solutions/Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,11 @@
+using System;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+    }
+}
