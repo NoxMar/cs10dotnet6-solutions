56dcfa29452fc49759d256d5a506e8d9fcaa5f62
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-25 22:28:33 +0100

    feat: set and read values of fields in Person class

---
+++ solutions/Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+Person bob = new();
+WriteLine(bob.ToString());
+
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new(1965, 12, 22);
+WriteLine($"{bob.Name} was born on {bob.DateOfBirth:dddd, d MMMM yyyy}");
+
+Person alice = new() { Name = "Alice Jones", DateOfBirth = new(1998, 3, 7) };
+
+WriteLine($"{alice.Name} was born on {alice.DateOfBirth:dd MMM yy}");
