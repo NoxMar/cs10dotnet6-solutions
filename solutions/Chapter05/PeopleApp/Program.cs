b49a4d3da5b5ffae82f2e1c3c2237e7613871ba4
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-26 22:03:03 +0100

    feat: demonstrate use of and enum for favorite wonder

--- solutions/Chapter05/PeopleApp/Program.cs
+++ solutions/Chapter05/PeopleApp/Program.cs
@@ -11,3 +11,8 @@ WriteLine($"{bob.Name} was born on {bob.DateOfBirth:dddd, d MMMM yyyy}");
 Person alice = new() { Name = "Alice Jones", DateOfBirth = new(1998, 3, 7) };
 
 WriteLine($"{alice.Name} was born on {alice.DateOfBirth:dd MMM yy}");
+
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+
+WriteLine($"{bob.Name}'s favorite wonder is {bob.FavoriteAncientWonder}, Its integer is {(int)bob.FavoriteAncientWonder}");
