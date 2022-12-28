4ccf04d25f53792f6aad47e4e70fc8eb58591ddd
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-28 21:26:47 +0100

    feat: add initial project structure for 6th chapter

---
+++ solutions/Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person : object
+{
+    // fields
+    public string? Name; // ? - nullable
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+}
