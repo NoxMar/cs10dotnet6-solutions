e1e58e5925dacc1fe7748072da77c6292a0e3f38
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-28 21:38:38 +0100

    feat: add ability to procreate

--- solutions/Chapter06/PeopleApp/Program.cs
+++ solutions/Chapter06/PeopleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person marry = new() { Name = "Marry" };
+Person jill = new() { Name = "Jill" };
+// call instance method
+Person baby1 = marry.ProcreateWith(harry);
+baby1.Name = "Gary";
+// call static method
+Person baby2 = Person.Procreate(harry, jill);
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{marry.Name} has {marry.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+
+WriteLine($"{harry.Name}'s first child is named \"{harry.Children[0].Name}\"");
