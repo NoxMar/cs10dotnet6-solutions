0680ff6e2946878b992b3e47ac7d0d1dd78463b5
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-28 22:17:25 +0100

    feat: demonstrate usage of non-generic collections

--- solutions/Chapter06/PeopleApp/Program.cs
+++ solutions/Chapter06/PeopleApp/Program.cs
@@ -33,3 +33,12 @@ static void Harry_Shout(object? sender, EventArgs e)
     Person p = (Person)sender;
     WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
 }
+
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(1, "Alpha");
+lookupObject.Add(2, "Beta");
+lookupObject.Add(3, "Gamma");
+lookupObject.Add(harry, "Delta");
+int key = 2;
+WriteLine($"Key {key} has value: {lookupObject[key]}");
+WriteLine($"Key {harry} has value: {lookupObject[harry]}");
