9d7bcf4fc5c60e616b3dd660ab69dbf01737d483
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2022-12-29 21:35:30 +0100

    feat: add DisplacementVector implementation with op+

---
+++ solutions/Chapter06/PacktLibrary/DisplacementVector.cs
@@ -0,0 +1,20 @@
+namespace Packt.Shared;
+
+public struct DisplacementVector
+{
+    public int X;
+    public int Y;
+    public DisplacementVector(int initialX, int initialY)
+    {
+        X = initialX;
+        Y = initialY;
+    }
+
+    public static DisplacementVector operator +(
+        DisplacementVector v1,
+        DisplacementVector v2
+    )
+    {
+        return new(v1.X + v2.X, v1.Y + v2.Y);
+    }
+}
