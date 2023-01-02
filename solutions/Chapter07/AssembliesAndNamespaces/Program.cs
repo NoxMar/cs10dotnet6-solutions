a9c89228e0170fcea5c60a6d8462173e0fdfb657
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-02 22:23:34 +0100

    feat: create AssembiesAndNamespaces project

---
+++ solutions/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq; // XDocument
+using System; // String
+
+using static System.Console;
+
+Console.WriteLine("Hello, World!");
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
