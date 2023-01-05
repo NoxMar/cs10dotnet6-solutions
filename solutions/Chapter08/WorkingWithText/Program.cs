4e0b1855446d078ab9b161716cffd67c8b592be8
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-05 13:13:19 +0100

    feat: add project represeinpresenting simple string usage

---
+++ solutions/Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+string city = "London";
+WriteLine($"{city} is {city.Length} characters long.");
+
+WriteLine($"First char is {city[0]} and third is {city[2]}.");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellín";
+string[] citiesArray = cities.Split(',');
+WriteLine($"There are {citiesArray.Length} items in the array.");
+foreach (string item in citiesArray)
+{
+    WriteLine(item);
+}
+
+string recombined = string.Join(" => ", citiesArray);
+WriteLine(recombined);
+
+string fruit = "Apples";
+decimal price = 0.39M;
+DateTime when = DateTime.Today;
+WriteLine($"Interpolated:  {fruit} cost {price:C} on {when:dddd}.");
+WriteLine(string.Format("string.Format: {0} cost {1:C} on {2:dddd}.",
+  arg0: fruit, arg1: price, arg2: when));
