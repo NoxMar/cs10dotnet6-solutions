9b98ac6905d181459d444ba443a822b2b79a6ce1
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-10 21:31:47 +0100

    feat: parse out specified parts out of an URL

---
+++ solutions/Chapter08/WorkingWithNetworkResources/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+using static System.Console;
+
+Write("Enter a valid web address: ");
+string? url = ReadLine();
+if (string.IsNullOrWhiteSpace(url))
+{
+    url = "https://stackoverflow.com/search?q=securestring";
+}
+Uri uri = new(url);
+WriteLine($"URL: {url}");
+WriteLine($"Scheme: {uri.Scheme}");
+WriteLine($"Port: {uri.Port}");
+WriteLine($"Host: {uri.Host}");
+WriteLine($"Path: {uri.AbsolutePath}");
+WriteLine($"Query: {uri.Query}");
