4231eead461dd11e52b3325a80698a9775e211a4
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-11 22:33:02 +0100

    feat: inspect member-level attributes via reflection

--- solutions/Chapter08/WorkingWithReflection/Program.cs
+++ solutions/Chapter08/WorkingWithReflection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection; // Assembly
+using System.Runtime.CompilerServices; // CompilerGeneratedAttribute
+using Packt.Shared; // CoderAttribute
 
 using static System.Console;
 
@@ -24,3 +26,38 @@ WriteLine($"  Version: {version?.InformationalVersion}");
 AssemblyCompanyAttribute? company = assembly
   .GetCustomAttribute<AssemblyCompanyAttribute>();
 WriteLine($"  Company: {company?.Company}");
+
+WriteLine();
+WriteLine($"* Types:");
+Type[] types = assembly.GetTypes();
+foreach (Type type in types)
+{
+    WriteLine();
+    WriteLine($"Type: {type.FullName}");
+    MemberInfo[] members = type.GetMembers();
+    foreach (MemberInfo member in members)
+    {
+        WriteLine("{0}: {1} ({2})",
+          arg0: member.MemberType,
+          arg1: member.Name,
+          arg2: member.DeclaringType?.Name);
+        IOrderedEnumerable<CoderAttribute> coders =
+          member.GetCustomAttributes<CoderAttribute>()
+          .OrderByDescending(c => c.LastModified);
+        foreach (CoderAttribute coder in coders)
+        {
+            WriteLine("-> Modified by {0} on {1}",
+              coder.Coder, coder.LastModified.ToShortDateString());
+        }
+    }
+}
+
+class Animal
+{
+    [Coder("Mark Price", "22 August 2021")]
+    [Coder("Johnni Rasmussen", "13 September 2021")]
+    public void Speak()
+    {
+        WriteLine("Woof...");
+    }
+}
