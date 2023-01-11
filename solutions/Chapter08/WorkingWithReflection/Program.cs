aea7c10eef8f601e4f862e7a44562c69a6bc4cc2
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-11 21:59:08 +0100

    feat: output assembly-level atributes using reflection

---
+++ solutions/Chapter08/WorkingWithReflection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection; // Assembly
+
+using static System.Console;
+
+WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();
+if (assembly is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+WriteLine($"  Full name: {assembly.FullName}");
+WriteLine($"  Location: {assembly.Location}");
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+WriteLine($"  Assembly-level attributes:");
+foreach (Attribute a in attributes)
+{
+    WriteLine($"   {a.GetType()}");
+}
+
+AssemblyInformationalVersionAttribute? version = assembly
+  .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+WriteLine($"  Version: {version?.InformationalVersion}");
+AssemblyCompanyAttribute? company = assembly
+  .GetCustomAttribute<AssemblyCompanyAttribute>();
+WriteLine($"  Company: {company?.Company}");
