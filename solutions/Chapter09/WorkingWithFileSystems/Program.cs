bbba8244322650b2bf765ff6403ed865980699fb
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-17 22:49:45 +0100

    feat: demonstrate basics of working with directories

--- solutions/Chapter09/WorkingWithFileSystems/Program.cs
+++ solutions/Chapter09/WorkingWithFileSystems/Program.cs
@@ -4,7 +4,8 @@ using static System.Environment;
 using static System.IO.Path;
 
 // OutputFileSystemInfo();
-WorkWithDrives();
+// WorkWithDrives();
+WorkWithDirectories();
 
 static void OutputFileSystemInfo()
 {
@@ -49,4 +50,26 @@ static void WorkWithDrives()
             WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
         }
     }
+}
+
+static void WorkWithDirectories()
+{
+    // define a directory path for a new folder
+    // starting in the user's folder
+    string newFolder = Combine(
+      GetFolderPath(SpecialFolder.Personal),
+      "Code", "Chapter09", "NewFolder");
+    WriteLine($"Working with: {newFolder}");
+    // check if it exists
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    // create directory 
+    WriteLine("Creating it...");
+    CreateDirectory(newFolder);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    Write("Confirm the directory exists, and then press ENTER: ");
+    ReadLine();
+    // delete directory 
+    WriteLine("Deleting it...");
+    Delete(newFolder, recursive: true);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
 }
