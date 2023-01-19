a07794d71bbd224497084e507f0a168e7b8c9524
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-19 23:57:28 +0100

    feat: demonstrate basics of working with files

---
+++ solutions/Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+WorkWithText();
+
+static void WorkWithText()
+{
+    string textFile = Combine(CurrentDirectory, "streams.txt");
+    StreamWriter text = File.CreateText(textFile);
+    foreach (string item in Viper.Callsigns)
+    {
+        text.WriteLine(item);
+    }
+    text.Close(); //releases the resource and **flushes**
+
+    WriteLine("{0} contains {1:N0} bytes",
+              textFile, new FileInfo(textFile).Length);
+    WriteLine(File.ReadAllText(textFile));
+}
+static class Viper
+{
+    // define an array of Viper pilot call signs
+    public static string[] Callsigns = new[]
+    {
+        "Husker", "Starbuck", "Apollo", "Boomer",
+        "Bulldog", "Athena", "Helo", "Racetrack"
+    };
+}
