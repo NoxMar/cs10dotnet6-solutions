cebefb856caeb3f8d307737478b33e3712db318e
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-20 00:09:08 +0100

    feat: demonstrate basics of working with xml files

--- solutions/Chapter09/WorkingWithStreams/Program.cs
+++ solutions/Chapter09/WorkingWithStreams/Program.cs
@@ -4,7 +4,8 @@ using static System.Console;
 using static System.Environment;
 using static System.IO.Path;
 
-WorkWithText();
+// WorkWithText();
+WorkWithXml();
 
 static void WorkWithText()
 {
@@ -20,6 +21,35 @@ static void WorkWithText()
               textFile, new FileInfo(textFile).Length);
     WriteLine(File.ReadAllText(textFile));
 }
+
+static void WorkWithXml()
+{
+    // define file path to write to
+    string xmlFile = Combine(CurrentDirectory, "streams.xml");
+    // creates file stream (and, also file)
+    FileStream xmlFileStream = File.Create(xmlFile);
+    // warp the file stream in an XML writer helper and automatically
+    // indent nested elements
+    XmlWriter xml = XmlWriter.Create(xmlFileStream,
+        new XmlWriterSettings { Indent = true });
+
+    xml.WriteStartDocument();
+    xml.WriteStartElement("callsigns");
+    foreach (string callsing in Viper.Callsigns)
+    {
+        xml.WriteElementString("callsign", callsing);
+    }
+    xml.WriteEndElement();
+    // WARNING: closing helper doesn't close the file stream, apparently
+    xml.Close();
+    xmlFileStream.Close();
+
+    WriteLine("{0} contains {1:N0} bytes.",
+        xmlFile,
+        new FileInfo(xmlFile).Length);
+    WriteLine(File.ReadAllText(xmlFile));
+}
+
 static class Viper
 {
     // define an array of Viper pilot call signs
