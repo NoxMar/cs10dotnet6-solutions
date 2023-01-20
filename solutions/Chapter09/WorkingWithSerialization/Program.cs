2e9a03dad013c5a5e6b27a97136012442223054e
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-20 21:17:24 +0100

    feat: demonstrate basic obejct xml serialization

---
+++ solutions/Chapter09/WorkingWithSerialization/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Serialization; // XmlSerializer
+using Packt.Shared; // Person
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+List<Person> people = new()
+{
+    new(30_000M)
+    {
+        FirstName = "Alice",
+        LastName = "Smith",
+        DateOfBirth = new(1974, 3, 14),
+    },
+    new(40_000M)
+    {
+        FirstName = "Bob",
+        LastName = "Jones",
+        DateOfBirth = new(1969, 11, 23),
+    },
+    new(20_00M)
+    {
+        FirstName = "Charlie",
+        LastName = "Cox",
+        DateOfBirth = new(1984, 5, 4),
+        Children = new ()
+        {
+            new(0M)
+            {
+                FirstName = "Sally",
+                LastName = "Cox",
+                DateOfBirth = new(2000, 7, 12),
+            },
+        },
+    },
+};
+
+XmlSerializer xs = new(people.GetType());
+string path = Combine(CurrentDirectory, "people.xml");
+using (FileStream stream = File.Create(path))
+{
+    xs.Serialize(stream, people);
+}
+
+WriteLine("Written {0:N0} bytes of XML to {1}",
+    new FileInfo(path).Length,
+    path);
+WriteLine();
+
+WriteLine(File.ReadAllText(path));
