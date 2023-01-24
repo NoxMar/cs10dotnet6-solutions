dfef3d8ab38b95e5cf7c8c1c243545355196725c
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-24 20:44:15 +0100

    feat: query db for categories in their contents

--- solutions/Chapter10/WorkingWithEFCore/Program.cs
+++ solutions/Chapter10/WorkingWithEFCore/Program.cs
@@ -1,5 +1,27 @@
-﻿using Packt.Shared;
+﻿using Microsoft.EntityFrameworkCore;
+
+using Packt.Shared;
 
 using static System.Console;
 
 WriteLine($"Using {ProjectConstants.DatabaseProvider} databse provider.");
+QueryingCategories();
+
+static void QueryingCategories()
+{
+    using Northwind db = new();
+    WriteLine("Categories and how many products they have:");
+    IQueryable<Category>? categories = db.Categories?
+        .Include(c => c.Products);
+    if (categories is null)
+    {
+        WriteLine("No categories found.");
+        return;
+    }
+
+    // execute query and enumerate results
+    foreach (Category c in categories)
+    {
+        WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+    }
+}
