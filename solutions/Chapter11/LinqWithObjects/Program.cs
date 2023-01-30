2c9f1f6e143bfac256ada120886aa09001b61e0f
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-30 23:28:47 +0100

    feat(ch11): demonstrate deffered execution of LINQ queries

---
+++ solutions/Chapter11/LinqWithObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+// a string array is a sequence that implements IEnumerable<string>
+string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin",
+    "Toby", "Creed" };
+WriteLine("Deferred execution");
+// Question: Which names end with an M?
+
+// (written with LINQ extension methods)
+var query1 = names.Where(name => name.EndsWith("m"));
+
+// (written with LINQ query comprehension syntax)
+var query2 = from name in names where name.EndsWith("m") select name;
+
+// Getting answers
+string[] result1 = query1.ToArray();
+
+List<string> result2 = query2.ToList();
+
+foreach (string name in query1)
+{
+    WriteLine(name); // outputs Pam
+    names[2] = "Jimmy"; // change Jim to Jimmy to show that finding next element is deferred
+    // by changing before the second iteration and showing that original "Jim" is not returned.
+}
