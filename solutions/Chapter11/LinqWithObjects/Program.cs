501b9dd5c7f3bbe1d4a8b393ca6bd0833422fc62
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-01-31 22:50:45 +0100

    feat(ch11): order names by length then alphabetically

--- solutions/Chapter11/LinqWithObjects/Program.cs
+++ solutions/Chapter11/LinqWithObjects/Program.cs
@@ -28,7 +28,10 @@ WriteLine("Writing queries:");
 // With explicit delegate instantiation:
 // var query = names.Where(new Func<string, bool>(NameLongerThanFour));
 
-var query = names.Where(name => name.Length > 4);
+var query = names
+    .Where(name => name.Length > 4)
+    .OrderBy(name => name.Length)
+    .ThenBy(name => name);
 
 foreach (string item in query)
 {
