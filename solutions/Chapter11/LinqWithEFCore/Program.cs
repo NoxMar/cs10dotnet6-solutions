675a27ce4524f42f36adf0485fd9892f1ad1a829
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-01 23:13:58 +0100

    feat(ch11): demonstrate flitering and qurying using linq and ef core

--- solutions/Chapter11/LinqWithEFCore/Program.cs
+++ solutions/Chapter11/LinqWithEFCore/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared; // Northwind, Category, Product
+using Microsoft.EntityFrameworkCore; // DbSet<T>
+using static System.Console;
+
+FilterAndSort();
+
+static void FilterAndSort()
+{
+    using Northwind db = new();
+    DbSet<Product> allProducts = db.Products!;
+    IQueryable<Product> filteredProduct =
+        allProducts.Where(product => product.UnitPrice < 10M);
+    IOrderedQueryable<Product> sortedAndFilteredProducts =
+        filteredProduct.OrderByDescending(product => product.UnitPrice);
+    WriteLine("Products that cost less than $10:");
+    foreach (Product p in sortedAndFilteredProducts)
+    {
+        WriteLine(
+            $"{p.ProductId}: {p.ProductName} costs {p.UnitPrice:$#,##0.00}");
+    }
+    WriteLine();
+}
