ce8b2330efae7f8ce661697de86b78735734b6a7
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-05 18:29:12 +0100

    feat(ch11e02): output customers in the specified city

---
+++ solutions/Chapter11/Exercise02/Db/NorthwindContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Exercise02.Db
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+        }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Filename=Northwind.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasIndex(e => e.City, "City");
+
+                entity.HasIndex(e => e.CompanyName, "CompanyNameCustomers");
+
+                entity.HasIndex(e => e.PostalCode, "PostalCodeCustomers");
+
+                entity.HasIndex(e => e.Region, "Region");
+
+                entity.Property(e => e.CustomerId).HasColumnType("nchar (5)");
+
+                entity.Property(e => e.Address).HasColumnType("nvarchar (60)");
+
+                entity.Property(e => e.City).HasColumnType("nvarchar (15)");
+
+                entity.Property(e => e.CompanyName).HasColumnType("nvarchar (40)");
+
+                entity.Property(e => e.ContactName).HasColumnType("nvarchar (30)");
+
+                entity.Property(e => e.ContactTitle).HasColumnType("nvarchar (30)");
+
+                entity.Property(e => e.Country).HasColumnType("nvarchar (15)");
+
+                entity.Property(e => e.Fax).HasColumnType("nvarchar (24)");
+
+                entity.Property(e => e.Phone).HasColumnType("nvarchar (24)");
+
+                entity.Property(e => e.PostalCode).HasColumnType("nvarchar (10)");
+
+                entity.Property(e => e.Region).HasColumnType("nvarchar (15)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ solutions/Chapter11/Exercise02/Program.cs
@@ -0,0 +1,30 @@
+﻿using Exercise02.Db;
+
+using static System.Console;
+
+Exercise2();
+
+static void Exercise2()
+{
+    using NorthwindContext db = new();
+    var city = ReadCity();
+    var customersInCity = db.Customers
+        .Where(c => c.City == city);
+    if (customersInCity is null)
+    {
+        WriteLine($"There were no customers found in {city}");
+        return;
+    }
+
+    WriteLine($"There are {customersInCity.Count()} customers in {city}:");
+    foreach (var c in customersInCity)
+    {
+        WriteLine(c.CompanyName);
+    }
+}
+
+static string ReadCity()
+{
+    Write("Enter the name of a city: ");
+    return ReadLine()!;
+}
