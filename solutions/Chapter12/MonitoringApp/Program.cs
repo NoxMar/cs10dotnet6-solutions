2e30b2390c214b446a66a251f4805ccb943521e4
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-05 23:20:08 +0100

    feat(ch12): add simple benchmarking type

--- solutions/Chapter12/MonitoringApp/Program.cs
+++ solutions/Chapter12/MonitoringApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MonitoringLib; // Recorder
+using static System.Console;
+WriteLine("Processing. Please wait...");
+Recorder.Start();
+// simulate a process that requires some memory resources...
+int[] largeArrayOfInts = Enumerable.Range(
+  start: 1, count: 10_000).ToArray();
+// ...and takes some time to complete
+Thread.Sleep(new Random().Next(5, 10) * 1000);
+Recorder.Stop();
