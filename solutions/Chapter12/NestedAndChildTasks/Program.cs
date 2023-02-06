25a6432da59cd0769b6118f2d8dd7a80d1db872c
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-06 22:24:42 +0100

    feat(ch12): demonstrate creating a nested task
    It's worth noting that parrent task is allowed to finish before a nested task
    finishes. To change this behavior use `TaskCreationOptions.AttachedToParent`
    making it a child task.

---
+++ solutions/Chapter12/NestedAndChildTasks/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Task outerTask = Task.Factory.StartNew(OuterMethod);
+outerTask.Wait();
+WriteLine("Console app is stopping.");
+
+static void OuterMethod()
+{
+    WriteLine("Outer method starting...");
+    Task innerTask = Task.Factory.StartNew(InnerMethod);
+    WriteLine("Outer task finished");
+}
+
+static void InnerMethod()
+{
+    WriteLine("Inner method starting...");
+    Thread.Sleep(2000);
+    WriteLine("Inner method finished");
+}
