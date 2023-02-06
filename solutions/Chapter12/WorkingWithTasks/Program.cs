c77bffefca3816f32b3747ec20c4fa67dbc83f61
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-06 22:13:14 +0100

    feat(ch12): demonstrate using task conitnuation

--- solutions/Chapter12/WorkingWithTasks/Program.cs
+++ solutions/Chapter12/WorkingWithTasks/Program.cs
@@ -9,12 +9,19 @@ MethodA();
 MethodB();
 MethodC();
 */
-WriteLine("Running methods asynchronously on multiple threads");
+/*WriteLine("Running methods asynchronously on multiple threads");
 Task taskA = new(MethodA);
 taskA.Start();
 Task taskB = Task.Factory.StartNew(MethodB);
 Task taskC = Task.Run(MethodC);
 Task.WaitAll(taskA, taskB, taskC);
+*/
+
+WriteLine("Passing the result of one task as an input into another.");
+Task<string> taskServiceThenSProc = Task.Factory
+    .StartNew(CallWebService)
+    .ContinueWith(previousTask => CallStoredProcedure(previousTask.Result));
+WriteLine($"Result: {taskServiceThenSProc.Result}");
 WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
 
 static void OutputThreadInfo()
@@ -49,4 +56,22 @@ static void MethodC()
     OutputThreadInfo();
     Thread.Sleep(1000); // simulates 1 s of work
     WriteLine("Finished method C.");
+}
+
+static decimal CallWebService()
+{
+    WriteLine("Starting call to web service...");
+    OutputThreadInfo();
+    Thread.Sleep((new Random()).Next(2000, 4000));
+    WriteLine("Finished call to web service.");
+    return 89.99M;
+}
+
+static string CallStoredProcedure(decimal amount)
+{
+    WriteLine("Starting call to stored procedure...");
+    OutputThreadInfo();
+    Thread.Sleep((new Random()).Next(2000, 4000));
+    WriteLine("Finished call to stored procedure.");
+    return $"12 products cost more than {amount:C}";
 }
