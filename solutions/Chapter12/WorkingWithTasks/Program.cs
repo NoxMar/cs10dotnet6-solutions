1497ced5393043907373b5d58171e5205aca5149
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-06 21:51:04 +0100

    feat(ch12): execute task synchronously as a baseline

---
+++ solutions/Chapter12/WorkingWithTasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+OutputThreadInfo();
+Stopwatch timer = Stopwatch.StartNew();
+WriteLine("Running methods synchronously on one thread.");
+MethodA();
+MethodB();
+MethodC();
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+static void OutputThreadInfo()
+{
+    Thread t = Thread.CurrentThread;
+    WriteLine("Thread Id: {0}, Priority: {1}, Background: {2}, Name: {3}",
+        t.ManagedThreadId,
+        t.Priority,
+        t.IsBackground,
+        t.Name ?? "null");
+}
+
+static void MethodA()
+{
+    WriteLine("Starting Method A...");
+    OutputThreadInfo();
+    Thread.Sleep(3000); // simulates 3 s of work
+    WriteLine("Finished Method A.");
+}
+
+static void MethodB()
+{
+    WriteLine("Starting Method B...");
+    OutputThreadInfo();
+    Thread.Sleep(2000); // simulates 2 s of work
+    WriteLine("Finished method B.");
+}
+
+static void MethodC()
+{
+    WriteLine("Starting Method C...");
+    OutputThreadInfo();
+    Thread.Sleep(1000); // simulates 1 s of work
+    WriteLine("Finished method C.");
+}
