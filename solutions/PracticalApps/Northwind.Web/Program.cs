f58ee38bf9bd0dbf500f4a5f787b09710a9ce47c
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-12 21:47:04 +0100

    feat(ch14): enforce https through hsts

--- solutions/PracticalApps/Northwind.Web/Program.cs
+++ solutions/PracticalApps/Northwind.Web/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
 app.MapGet("/", () => "Hello World!");
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
 
 app.Run();
 Console.WriteLine("This executes after the web server has stopped!");
