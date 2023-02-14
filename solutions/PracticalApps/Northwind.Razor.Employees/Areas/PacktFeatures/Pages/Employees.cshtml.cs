b15fb27b0de30be4e7a4f3b59ffaa213379bdb8a
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-14 07:10:30 +0100

    feat(ch14): add view listing emoyees using razor classlib

---
+++ solutions/PracticalApps/Northwind.Razor.Employees/Areas/PacktFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared; // 
+
+// ReSharper disable once CheckNamespace 
+namespace Northwind.Razor.Employees.Pages;
+
+public class EmployeesPageModel : PageModel
+{
+    private NorthwindContext _db;
+    public Employee[] Employees { get; set; } = null!;
+    public EmployeesPageModel(NorthwindContext db)
+    {
+        _db = db;
+    }
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind B2B - Employees";
+        Employees = _db.Employees
+            .OrderBy(e => e.LastName)
+            .ThenBy(e => e.FirstName)
+            .ToArray();
+    }
+}
