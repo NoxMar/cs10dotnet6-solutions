4d94bee3d770e5f96d70129687b0abcd9c8b12cb
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-15 20:55:51 +0100

    feat(14.2): add order list to customer details

--- solutions/PracticalApps/Northwind.Web/Pages/Customer.cshtml.cs
+++ solutions/PracticalApps/Northwind.Web/Pages/Customer.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Packt.Shared;
 
 namespace Northwind.Web.Pages;
@@ -14,7 +15,10 @@ public class CustomerModel : PageModel
     }
     public async Task<IActionResult> OnGet(string id)
     {
-        var customer = await _db.Customers.FindAsync(id);
+        var customer = await _db.Customers
+            .Where(c => c.CustomerId == id)
+            .Include(c => c.Orders)
+            .FirstOrDefaultAsync();
         if (customer is null)
         {
             return Redirect("/customers");
