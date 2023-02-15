936ec92edafaee1be1c3b717752ad039edb6e2ed
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-15 20:45:55 +0100

    feat(14.2): add page showing customer details

--- solutions/PracticalApps/Northwind.Web/Pages/Customer.cshtml.cs
+++ solutions/PracticalApps/Northwind.Web/Pages/Customer.cshtml.cs
@@ -1,11 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
 
 namespace Northwind.Web.Pages;
 
-public class Customer : PageModel
+public class CustomerModel : PageModel
 {
-    public void OnGet()
+    private NorthwindContext _db;
+    public Customer Customer = null!;
+    public CustomerModel(NorthwindContext db)
     {
-        
+        _db = db;
+    }
+    public async Task<IActionResult> OnGet(string id)
+    {
+        var customer = await _db.Customers.FindAsync(id);
+        if (customer is null)
+        {
+            return Redirect("/customers");
+        }
+
+        Customer = customer;
+
+        return Page();
     }
 }
