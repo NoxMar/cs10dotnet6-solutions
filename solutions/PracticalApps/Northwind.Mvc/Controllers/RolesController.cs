0f747ca4112c8c7953142edc7eedf11925c53e28
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-16 23:22:21 +0100

    feat(ch15): demonstrate programatic role managment

---
+++ solutions/PracticalApps/Northwind.Mvc/Controllers/RolesController.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Identity; // RoleManager, UserManager
+using Microsoft.AspNetCore.Mvc; // Controller, IActionResult
+using static System.Console;
+namespace Northwind.Mvc.Controllers;
+
+public class RolesController : Controller
+{
+  private const string AdminRole = "Administrators";
+  private const string UserEmail = "test@example.com";
+  private readonly RoleManager<IdentityRole> _roleManager;
+  private readonly UserManager<IdentityUser> _userManager;
+  public RolesController(RoleManager<IdentityRole> roleManager,
+    UserManager<IdentityUser> userManager)
+  {
+    _roleManager = roleManager;
+    _userManager = userManager;
+  }
+  public async Task<IActionResult> Index()
+  {
+    if (!(await _roleManager.RoleExistsAsync(AdminRole)))
+    {
+      await _roleManager.CreateAsync(new IdentityRole(AdminRole));
+    }
+    IdentityUser user = await _userManager.FindByEmailAsync(UserEmail);
+    if (user == null)
+    {
+      user = new();
+      user.UserName = UserEmail;
+      user.Email = UserEmail;
+      IdentityResult result = await _userManager.CreateAsync(
+        user, "Pa$$w0rd");
+      if (result.Succeeded)
+      {
+        WriteLine($"User {user.UserName} created successfully.");
+      }
+      else
+      { 
+        foreach (IdentityError error in result.Errors)
+        {
+          WriteLine(error.Description);
+        }
+      }
+    }
+    if (!user.EmailConfirmed)
+    {
+      string token = await _userManager
+        .GenerateEmailConfirmationTokenAsync(user);
+      IdentityResult result = await _userManager
+        .ConfirmEmailAsync(user, token);
+      if (result.Succeeded)
+      {
+        WriteLine($"User {user.UserName} email confirmed successfully.");
+      }
+      else
+      {
+        foreach (IdentityError error in result.Errors)
+        {
+          WriteLine(error.Description);
+        }
+      }
+    }
+    if (!(await _userManager.IsInRoleAsync(user, AdminRole)))
+    {
+      IdentityResult result = await _userManager
+        .AddToRoleAsync(user, AdminRole);
+      if (result.Succeeded)
+      {
+        WriteLine($"User {user.UserName} added to {AdminRole} successfully.");
+      }
+      else
+      {
+        foreach (IdentityError error in result.Errors)
+        {
+          WriteLine(error.Description);
+        }
+      }
+    }
+    return Redirect("/");
+  }
+}
