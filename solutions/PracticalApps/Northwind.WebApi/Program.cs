a6dd5902800283b26cf6e38a83adcedc08acbc18
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-21 21:38:04 +0100

    feat(ch16): log formatters on startup

--- solutions/PracticalApps/Northwind.WebApi/Program.cs
+++ solutions/PracticalApps/Northwind.WebApi/Program.cs
@@ -1,10 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Packt.Shared; // AddNorthwindContext extension method
+using static System.Console;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddNorthwindContext();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    foreach (var formatter in options.OutputFormatters)
+    {
+        var mediaFormatter = formatter as OutputFormatter;
+        if (mediaFormatter is null)
+        {
+            WriteLine($"\t{formatter.GetType().Name}");
+        }
+        else // OutputFormatter class has SupportedMediaTypes
+        {
+            WriteLine("\t{0}, Media types: {1}",
+                arg0: mediaFormatter.GetType().Name,
+                arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes));
+        }
+    }
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
