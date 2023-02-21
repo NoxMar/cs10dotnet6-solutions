0ccebe86bf6ad0974c9b38f0d26b1468ec9d7c34
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-02-21 20:51:35 +0100

    feat(ch16): add specified amount of days forecast endpoint

--- solutions/PracticalApps/Northwind.WebApi/Controllers/WeatherForecastController.cs
+++ solutions/PracticalApps/Northwind.WebApi/Controllers/WeatherForecastController.cs
@@ -17,16 +17,26 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    
+    // GET /weatherforecast
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get() // original method
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
+        return Get(5); // five days by default
+    }
+
+    // GET /weatherforecast/7
+    [HttpGet("{days:int}")]
+    public IEnumerable<WeatherForecast> Get(int days)
+    {
+        return Enumerable.Range(1, days)
+            .Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
             .ToArray();
     }
 }
