daa487e76dcb3bae3cbe9551b92f4d6a4212eeb0
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-04 21:16:44 +0100

    feat(ch18): wire OData endpoints to controllers

--- solutions/PracticalApps/Northwind.OData/Program.cs
+++ solutions/PracticalApps/Northwind.OData/Program.cs
@@ -1,10 +1,33 @@
+using Microsoft.AspNetCore.OData; // AddOData extension method
+using Microsoft.OData.Edm; // IEdmModel
+using Microsoft.OData.ModelBuilder; // ODataConventionModelBuilder
+using Packt.Shared; // NorthwindContext and entity models
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("https://localhost:5004");
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddNorthwindContext();
+
+builder.Services.AddControllers()
+    // register OData models including multiple versions
+    .AddOData(options => options
+        // register OData models including multiple versions
+        .AddRouteComponents(routePrefix: "catalog",
+            model: GetEdmModelForCatalog())
+        .AddRouteComponents(routePrefix: "ordersystem",
+            model: GetEdmModelForOrderSystem())
+    
+        // enable query options
+        .Select() // enable $select
+        .Expand() // enable $expend
+        .Filter() // enable $filter
+        .OrderBy() // enable $orderby
+        .SetMaxTop(100) // enable $top
+        .Count() // enable $count
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,3 +48,23 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+IEdmModel GetEdmModelForCatalog()
+{
+    ODataConventionModelBuilder builder = new();
+    builder.EntitySet<Category>("Categories");
+    builder.EntitySet<Product>("Products");
+    builder.EntitySet<Supplier>("Suppliers");
+    return builder.GetEdmModel();
+}
+
+IEdmModel GetEdmModelForOrderSystem()
+{
+    ODataConventionModelBuilder builder = new();
+    builder.EntitySet<Customer>("Customers");
+    builder.EntitySet<Order>("Orders");
+    builder.EntitySet<Employee>("Employees");
+    builder.EntitySet<Product>("Products");
+    builder.EntitySet<Shipper>("Shippers");
+    return builder.GetEdmModel();
+}
